20fb3cb3a76440e995c11a3980285087fbb65e84
rinat <swatlprus@gmail.com>
2022-03-04 15:35:11 +0500

    #9 Добавил новые модели

--- core/admin.py
+++ core/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 import core.models
 
 admin.site.register(core.models.Company)
+admin.site.register(core.models.Founder)
+admin.site.register(core.models.DateFound)
+admin.site.register(core.models.Staff)
+
+
 # Register your models here.
--- core/models.py
+++ core/models.py
@@ -1,11 +1,51 @@
 from django.db import models
+from datetime import date
 
-class Company(models.Model):
-    name = models.CharField('Название', max_length=128)
-    city = models.CharField('Город', max_length=128)
-    rate = models.IntegerField('Рейтинг', blank=True, null=True)
+class Founder(models.Model):
+    name = models.CharField('Основатель', max_length=128)
+
+    class Meta:
+        verbose_name = 'Основатель'
+        verbose_name_plural = 'Основатели'
 
     def __str__(self):
         return self.name
 
-# Create your models here.
+
+class Company(models.Model):
+    founder = models.ForeignKey('core.Founder', on_delete=models.CASCADE, null=True, blank=True, verbose_name='Основатель')
+    name = models.CharField('Название', max_length=128)
+    city = models.CharField('Город', max_length=128)
+    staff_count = models.ForeignKey('core.Staff', on_delete=models.CASCADE, null=True, blank=True, verbose_name='Кол-во сотрудников')
+    rate = models.IntegerField('Рейтинг', blank=True, null=True)
+
+    class Meta:
+        ordering = ['-rate']
+        verbose_name = 'Компания'
+        verbose_name_plural = 'Компании'
+
+    def __str__(self):
+        return self.name
+
+
+class DateFound(models.Model):
+    date = models.DateField()
+    company = models.ManyToManyField('core.Company')
+
+    class Meta:
+        verbose_name = 'Дата основания'
+        verbose_name_plural = 'Даты оснований'
+
+    def __str__(self):
+        return f'{self.date}'
+
+
+class Staff(models.Model):
+    count = models.IntegerField('Кол-во сотрудников', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Сотрудник'
+        verbose_name_plural = 'Сотрудники'
+
+    def __str__(self):
+        return str(self.count)
