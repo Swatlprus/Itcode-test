e7aae1d493463f5cb92d6d2e1575a291cbf71921
rinat <swatlprus@gmail.com>
2022-03-24 13:31:36 +0500

    Добавил Docker файл

--- project/settings.py
+++ project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tar_gq+qizy)#zo(anv2(1e*1kq#ziu)t@7_qv_tce$#)n_&(s'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-tar_gq+qizy)#zo(anv2(1e*1kq#ziu)t@7_qv_tce$#)n_&(s')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.getenv('DB_PATH', BASE_DIR / 'db.sqlite3'),
     }
 }
 
