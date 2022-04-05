73d3dc3c03f85ae401ea26504ba4931e46f2d371
rinat <swatlprus@gmail.com>
2022-04-05 11:48:04 +0500

    Деплой

--- project/settings.py
+++ project/settings.py
@@ -80,12 +80,24 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.getenv('DB_PATH', BASE_DIR / 'db.sqlite3'),
+if os.getenv('POSTGRESQL_PASS'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'itcode_proj',
+            'USER': 'itcode_name',
+            'PASSWORD': os.getenv('POSTGRESQL_PASS'),
+            'HOST': 'db',
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.getenv('DB_PATH', BASE_DIR / 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
